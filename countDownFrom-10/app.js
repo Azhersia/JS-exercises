e042183df87c5ee821597e559b450baafe5d1768
Azhersia <alissa.ostergard@gmail.com>
2023-03-08 13:17:13 +0200

    Fixed according to instructions given

--- countDownFrom-10/app.js
+++ countDownFrom-10/app.js
@@ -4,14 +4,25 @@ const timeEl = document.querySelector('#number')
 
 // set interval
 let timer = setInterval(function timeCount() {
-  // current time - 1
-  timeLeft -= 1;
-  // time inner text replaced with current time
-  timeEl.innerText = timeLeft
+  if (timeLeft >= 3) {
+    // current time - 1
+    timeLeft -= 1;
+    // time inner text replaced with current time
+    timeEl.innerHTML = `${timeLeft} seconds`
 
-  // if time is 0 or less than 0 remove interval set text to "hello!"
-  if (timeLeft <= 0) {
-    clearInterval(timer)
-    timeEl.innerText = 'Hello!'
+  } else if (timeLeft >= 1) {
+    // current time - 1
+    timeLeft -= 1;
+    // time inner text replaced with current time
+    timeEl.innerHTML = `${timeLeft} second`
+
+    // if time is 0 or less than 0 remove interval set text to "hello!"
+    if (timeLeft <= 0) {
+      clearInterval(timer)
+      timeEl.innerText = 'Hello!'
+      timeEl.classList.add("done")
+      document.body.style.background = "red"
+    }
   }
+
 }, 1000)
